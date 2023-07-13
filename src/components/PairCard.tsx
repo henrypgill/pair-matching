e8c98d6c765c48b6ca508ca023889c5babb479fb
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-13 15:39:19 +0100

    wrote reducer function with dispatch for cards

--- src/components/PairCard.tsx
+++ src/components/PairCard.tsx
@@ -1,19 +1,38 @@
 
+
 interface Image {
     url: string;
     showing: boolean;
-    id: string;
+    id: number;
+    matched: boolean;
+    dispatchAction(action: Action): void;
 }
 
+interface Action {
+    type: string;
+    id: number;
+}
 
-export function PairCard({url, showing, id}: Image): JSX.Element {
+export function PairCard({url, showing, id, matched, dispatchAction}: Image): JSX.Element {
 
     const backOfCardURL = ""
 
-    return (
-        <button>
-            <img src={showing ? url : backOfCardURL} className={`image-card`}></img>
-        </button>
+    function handleShowCard(cardID: number) {
+        const action: Action = {
+            type: 'showCard',
+            id: cardID,
+          };
+        dispatchAction(action)
+    }
 
-    )
+
+    // <img src={showing ? url : backOfCardURL} className={`image-card`}></img>
+
+    if (matched || showing) {
+        return (
+            <button onClick={() => handleShowCard(id)}>{url}</button>
+    )} else {
+        return(
+            <button onClick={() => handleShowCard(id)}>hmm</button>
+    )}
 }
--- src/components/PairMatchingApp.tsx
+++ src/components/PairMatchingApp.tsx
@@ -1,35 +1,62 @@
-import {useState, useEffect } from "react"
+import {useState, useEffect, useReducer } from "react"
 import { PairCard } from "./PairCard";
 
 interface Image {
     url: string;
     showing: boolean;
-    id: string;
+    id: number;
+    matched: boolean;
+    dispatchAction(action: Action): void;
+}
+
+interface Action {
+    type: string;
+    id: number;
 }
 
 export function PairMatchingApp(): JSX.Element {
 
+    const cardTextArray: string[] = ['😅', '💩', '✊🏼', '🪖', '🐸', '🥧', '🥜', '🍿']
+    const initialImages: Image[] = cardTextArray.map((emoji: string): Image => {
+        return {
+            url: emoji,
+            showing: false,
+            id: cardTextArray.indexOf(emoji),
+            matched: false,
+        }
+    })
 
-    const fetchURL = (pokeID: string): string => `${pokeID}`;
-    const boardSize = 4;
-    useEffect( 
-        () => {
-            const fetchPromise= fetch(fetchURL).then((response) => response.json).then((result) => result)
-            for (let i = 0; i < boardSize; i++){
-                // const fetchResult = fetchPromise;
-            }
-            setImageArray((prev) => [fetchPromise])
-        },
-        []
-    )
+    // const fetchURL = (pokeID: string): string => `${pokeID}`;
+    // const boardSize = 4;
 
-    const 
+    const [cards, cardDispatch] = useReducer(cardReducer, initialImages)
 
-    const [imageArray, setImageArray] = useState<string[]>([]);
+    function cardReducer(imagesState: Image[], imageAction: Action): Image[] {
+
+        const newImage: Image = {
+            url: "",//imagesState.find((image: Image) => image.id === imageAction.id).url,
+            showing: false,
+            id: imageAction.id,
+            matched: false,
+            dispatchAction: (action: Action) => cardDispatch(action)
+        }
+
+        switch (imageAction.type) {
+            case 'show':
+                return initialImages//imagesState.map((image: Action) => action.id === image.id ? null : null)
+            case 'match':
+                return initialImages
+            default:
+                return initialImages;
+            break;
+        }
+    }
+
+
 
 
     return (
-        imageArray.map(PairCard)
+        cards.map(PairCard)
     )
 
 
